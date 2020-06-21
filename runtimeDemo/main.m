e1a3e3c8e58ed73da7d90346d856f7e9d978127f
lordpan <275689823@qq.com>
2020-06-21 16:08:19 +0800

    创建工程

---
+++ runtimeDemo/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  runtimeDemo
+//
+//  Created by lordpan on 2020/6/21.
+//  Copyright © 2020 lordpan. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
