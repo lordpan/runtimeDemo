17b4b8c6f4ad8333b9b64f5554c3b089e7a3378a
lordpan <275689823@qq.com>
2020-06-21 16:47:47 +0800

    runtime基础 1

--- runtimeDemo/main.m
+++ runtimeDemo/main.m
@@ -7,6 +7,41 @@
 //
 
 #import <Foundation/Foundation.h>
+#import <objc/runtime.h>
+#import <objc/message.h>
+
+void method(){
+    
+}
+void Demo1(){
+    //创建类
+    Class Book = objc_allocateClassPair([NSObject class], "Book", 0);
+     //    添加成员变量
+    class_addIvar(Book, "_title", sizeof(NSString *), log2(sizeof(NSString *)), @encode(NSString *));
+    class_addIvar(Book, "_price", sizeof(int), sizeof(int),@encode(int) );
+    
+    SEL sel = sel_registerName("publish:");
+    
+    class_addMethod(Book, sel, (IMP)method, "v@:@");
+    
+    objc_registerClassPair(Book);
+    
+    id book = [[Book alloc]init];
+    
+    [book setValue:@"孔乙己" forKey:@"tiele"];
+    
+    Ivar pirce = class_getInstanceVariable(book, "_pirce");
+    
+    object_setIvar(book, pirce, @99);
+    
+    book = nil;
+    objc_disposeClassPair(book);
+    
+    
+    
+    
+}
+
 
 int main(int argc, const char * argv[]) {
     @autoreleasepool {
